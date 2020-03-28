b1b80bcd60e75b53a7ab8236a35c2c3257fbd135
Amnah Kurnia <59590227+IxoraVi@users.noreply.github.com>
2020-03-28 19:48:22 +0800

    Add files via upload

---
+++ PrintIterasi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+/* Baca N, */
+/* Print i = 1 s/d N dengan ITERATE */
+public class PrintIterasi {
+	public static void main(String[] args) {
+		int N;
+		int i;
+		Scanner masukan=new Scanner(System.in);
+
+		System.out.print ("Nilai N >0 = "); /* Inisialisasi*/
+		N = masukan.nextInt();
+		i = 1; /* First Elmt */
+		System.out.print ("Print i dengan ITERATE : \n");
+		for (;;){
+			System.out.println(i); /* Proses */
+			if (i == N)
+			/* Kondisi Berhenti */ break;
+			else {
+				i++; /* Next Elmt */
+			}
+		} /* (i == N) */
+	}
+}
